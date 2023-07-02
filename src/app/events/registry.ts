62f38fef2522896ea3e1ba9ba5dd8022cb97e225
tdadadavid <davidtofunmidada@gmail.com>
2023-07-02 20:27:18 +0100

    clean registry

--- src/app/events/registry.ts
+++ src/app/events/registry.ts
@@ -1,4 +1,4 @@
-import { logger, mail } from "../../core";
+import { logger } from "../../core";
 
 
 /**
@@ -6,11 +6,6 @@ import { logger, mail } from "../../core";
  */
 export const register = {
   "app:up": () => logger.info("Server started successfully"),
-  "auth:forget:password": [
-      (p: {x: number})  => { return 0; },
-  ],
-  "appointment:booked": (p: string) => mail.send(p as any),
-  "user:joined:community": (p: string, q: number) => mail.send(p as any),
   "event:registeration:succesful": () => logger.info("Events listeners registered")
 } 
 
